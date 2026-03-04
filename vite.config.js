8e483d02789bcd6054f4e71c379a73d9fe700a1e
Pete <138059854+psime@users.noreply.github.com>
2026-03-04 00:14:40 +0000

    test deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/psime/d3vite',
+  base: '/d3vite/',
   plugins: [react()],
 })
 
